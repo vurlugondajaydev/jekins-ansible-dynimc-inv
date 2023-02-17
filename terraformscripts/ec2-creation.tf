368ff286f2e71b527df7a3c67a0e592202f5dde3
vurlugondajaydev <87870973+vurlugondajaydev@users.noreply.github.com>
2023-02-17 12:21:19 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -1,13 +1,13 @@
 provider "aws"{
-  region = "ap-southeast-2"
+  region = "us-east-1"
 }
 
 
 resource "aws_instance" "AWSInstance"{
-    ami = "ami-08cd0547bf85331e7"
+    ami = "ami-0c9978668f8d55984"
 	instance_type = "t2.micro"
-	key_name = "devopstrainingmss"
-	security_groups = ["launch-wizard-2"]
+	key_name = "instkey-1"
+	security_groups = "jenkins_SG"
 	tags = {
 	 Name = "tomcatservers"
 	}
