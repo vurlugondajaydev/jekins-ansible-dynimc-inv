5def9cdf18add0cbc272aa669d54af8e20f9c56a
vurlugondajaydev <87870973+vurlugondajaydev@users.noreply.github.com>
2023-02-17 13:14:03 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -4,7 +4,7 @@ provider "aws"{
 
 
 resource "aws_instance" "AWSInstance"{
-    ami = "ami-0c9978668f8d55984"
+        ami = "ami-0c9978668f8d55984"
 	instance_type = "t2.micro"
 	key_name = "instkey-1"
 	security_groups = ["jenkins_SG"]
